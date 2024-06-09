31f284f96202fd5d3f429d5e36c1cad365b75030
AjayKrishna-S <ajaykrishna.sak@gmail.com>
2024-06-09 19:57:57 +0530

    final arrengment

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 const { engine } = require('express-handlebars')
-const {loginPage, dashboard, loginProcess} = require('./controllers/userController')
 const path = require('path');
-
+const userRoutes = require('./routes/users')
 
 app.engine('hbs',engine({defaultLayout:false,extname:"hbs"}));
 app.set('view engine', 'hbs')
@@ -11,9 +10,7 @@ app.set('view engine', 'hbs')
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({extended:true})); 
 
-app.get('/',loginPage)
-app.get('/dashboard',dashboard)
-app.post('/login',loginProcess)
+app.use(userRoutes);
 
 app.listen(8000, ()=>{
     console.log('port 8000 running');
